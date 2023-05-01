0475d7dd73313bf02c2c39c263165520146f293f
Guri26 <110395213+Guri26@users.noreply.github.com>
2023-05-01 21:04:07 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,8 +3,8 @@ citizen.CreateThread(function()
         citizen.wait(0) -- do this every tick
 
         -- traffic and ped desity management (these things NEED to rUn every tick)
-        SetTrafficDensity(0.0) -- 0.0 = none, 1.0 = max
-        SetPedDensity(0.0) -- 0.0 = none, 1.0 = max
+        SetTrafficDensity(0.5) -- 0.0 = none, 1.0 = max
+        SetPedDensity(0.5) -- 0.0 = none, 1.0 = max
     end
 end)
 
